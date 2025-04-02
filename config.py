8afa9e7de5f5e1a462c92816b3e8af88f8a37c72
ArtourAman <dota2blademail@gmail.com>
2025-04-02 18:18:52 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-API_ID = 29325958  # Your Telegram API ID
-API_HASH = "63c4eebe7892232cd579fb5790ca7bd7"  # Your Telegram API Hash
-SESSION_NAME = "artour_session"  # Your Pyrogram session name
-SOURCE_CHANNEL = -1001799503100 # Replace with your source channel ID
-DEST_CHANNEL = -1002570648989
